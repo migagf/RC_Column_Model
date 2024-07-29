0c936a55e0d54502862c6dcb30e778b75323d658
miguelgomez-ucb <miguel.gomez.f@berkeley.edu>
2024-07-28 22:54:57 -0700

    updated files for remote jobs

---
+++ quoFEM_input/templatedir/driver.bat
@@ -0,0 +1,2 @@
+"C:/Program Files/SimCenter/quoFEM/applications/performUQ/templateSub/simCenterSub.exe"  params.in tmpSimCenter.params model_parameters.py
+python main.py force 1> ops.out 2>&1
---
+++ quoFEM_input/templatedir/model_parameters.py
@@ -0,0 +1,23 @@
+'''
+Model parameters to be updated
+'''
+
+gamma  = "RV.gamma"
+kappa  = "RV.kappa"
+
+eta1  = "RV.eta1"
+    
+sig  = "RV.sig"
+lam  = "RV.lam"
+    
+mup  = "RV.mup"
+sigp  = "RV.sigp"
+    
+rsmax  = "RV.rsmax"
+
+alpha  = "RV.alpha"
+alpha1  = "RV.alpha1"
+alpha2  = "RV.alpha2"
+    
+betam1  = "RV.betam1"
+
