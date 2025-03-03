1665c4dd6339a81770094c3ec91e30680e4051a2
miguelgomez-ucb <miguel.gomez.f@berkeley.edu>
2025-03-03 02:10:55 -0800

    updated slides

--- a_06_surrogate_performance.py
+++ a_06_surrogate_performance.py
@@ -8,6 +8,12 @@ from database_processing_functions import *
 from a_02_create_data_matrix import *
 import matplotlib.pyplot as plt
 
+
+# Set latex as intepreter for matplotlib
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+
 # Load the surrogate_perfomance data
 surrogate_performance = pd.read_csv('surrogate_performance_data_new.csv')
 
@@ -31,7 +37,10 @@ test_data = surrogate_performance[surrogate_performance['TestOrTrain'] == 'Test'
 # Plot properties
 dpi_plots = 100
 figuresize = (5, 5)
+
 colors = {'Flexure': 'b', 'Flexure-Shear': 'k', 'Shear': 'r'}
+color_labels_surrogate = {'GP - Flexure': 'b', 'GP - Flexure-Shear': 'k', 'GP - Shear': 'r'}
+color_labels_cals = {'Cal - Flexure': 'b', 'Cal - Flexure-Shear': 'k', 'Cal - Shear': 'r'}
 
 # Create a 3D scatter plot of rmse_sur 
 fig = plt.figure(dpi=dpi_plots, figsize=figuresize)
@@ -39,23 +48,38 @@ ax = fig.add_subplot(111, projection='3d')
 
 # Create a color map based on FailureType
 color_values = test_data['Type'].map(colors)
-ax.scatter(test_data[xpar], test_data[ypar], test_data['RMSESurr'], c=color_values, marker='o', label='Surrogate')
+# Create a 3D scatter plot of rmse_sur with color labels
+ax.scatter(test_data[xpar], test_data[ypar], test_data['RMSESurr'], c=color_values, marker='o')
+
+# Add color labels
+for label, color in color_labels_surrogate.items():
+    ax.scatter([], [], c=color, label=f'{label}')
+
 # Add rmse_cal
-ax.scatter(test_data[xpar], test_data[ypar], test_data['RMSECal'], c=color_values, marker='^', label='Calibration')  
+ax.scatter(test_data[xpar], test_data[ypar], test_data['RMSECal'], c=color_values, marker='^')  
 # Join both with vertical lines
 
+# Add color labels for calibration
+for label, color in color_labels_cals.items():
+    ax.scatter([], [], c=color, marker='^', label=f'{label}')
+    
+ax.legend(loc='upper left', fontsize=8)
+
+# Add lines connecting calibrated and surrogate points
 for i in range(len(test_data)):
     ax.plot([test_data[xpar].iloc[i], test_data[xpar].iloc[i]], 
             [test_data[ypar].iloc[i], test_data[ypar].iloc[i]], 
             [test_data['RMSESurr'].iloc[i], test_data['RMSECal'].iloc[i]], c='k', linestyle='--', linewidth=0.5)
     
-ax.legend()
 plt.title('RMSE on Test Data')
 ax.set_zlim(0, 0.015)
 #ax.set_ylim(0, 2)
 # Add labels for x and y axes
 ax.set_xlabel(xpar_label)
 ax.set_ylabel(ypar_label)
+
+# Set view angle to 45 degrees from all sides
+ax.view_init(elev=30, azim=-45)
 plt.show()
 
 
@@ -71,20 +95,35 @@ ax = fig.add_subplot(111, projection='3d')
 color_values = train_data['Type'].map(colors)
 
 # Create a 3D scatter plot of rmse_sur
-ax.scatter(train_data[xpar], train_data[ypar], train_data['RMSESurr'], c=color_values, marker='o', label='Surrogate')
+ax.scatter(train_data[xpar], train_data[ypar], train_data['RMSESurr'], c=color_values, marker='o')
+
+# Add color labels
+for label, color in color_labels_surrogate.items():
+    ax.scatter([], [], c=color, label=f'{label}')
+
 # Add rmse_cal
-ax.scatter(train_data[xpar], train_data[ypar], train_data['RMSECal'], c=color_values, marker='^', label='Calibration')
+ax.scatter(train_data[xpar], train_data[ypar], train_data['RMSECal'], c=color_values, marker='^')  
 # Join both with vertical lines
+
+# Add color labels for calibration
+for label, color in color_labels_cals.items():
+    ax.scatter([], [], c=color, marker='^', label=f'{label}')
+    
+ax.legend(loc='upper left', fontsize=8)
+
+# Add lines connecting calibrated and surrogate points
 for i in range(len(train_data)):
     ax.plot([train_data[xpar].iloc[i], train_data[xpar].iloc[i]], 
             [train_data[ypar].iloc[i], train_data[ypar].iloc[i]], 
             [train_data['RMSESurr'].iloc[i], train_data['RMSECal'].iloc[i]], c='k', linestyle='--', linewidth=0.5)
     
-ax.legend()
 plt.title('RMSE on Train Data')
 ax.set_zlim(0, 0.015)
 ax.set_xlabel(xpar_label)
 ax.set_ylabel(ypar_label)
+
+# Set view angle to 45 degrees from all sides
+ax.view_init(elev=30, azim=-45)
 plt.show()
 # ax.set_ylim(0, 2)
 
@@ -96,12 +135,23 @@ ax = fig.add_subplot(111, projection='3d')
 # Create a 3D scatter plot of rmse_sur
 
 # Create a color map based on FailureType
-
 color_values = all_data['Type'].map(colors)
 
 # Create a 3D scatter plot of rmse_sur with color based on FailureType
-ax.scatter(all_data[xpar], all_data[ypar], all_data['RMSESurr'], c=color_values, marker='o', label='Surrogate')
-ax.scatter(all_data[xpar], all_data[ypar], all_data['RMSECal'], c=color_values, marker='^', label='Calibration')
+ax.scatter(all_data[xpar], all_data[ypar], all_data['RMSESurr'], c=color_values, marker='o')
+
+# Add color labels
+for label, color in color_labels_surrogate.items():
+    ax.scatter([], [], c=color, label=f'{label}')
+
+# Add rmse_cal
+ax.scatter(all_data[xpar], all_data[ypar], all_data['RMSECal'], c=color_values, marker='^')  
+
+# Add color labels for calibration
+for label, color in color_labels_cals.items():
+    ax.scatter([], [], c=color, marker='^', label=f'{label}')
+    
+ax.legend(loc='upper left', fontsize=8)
 
 # Join both with vertical lines
 for i in range(len(all_data)):
@@ -109,11 +159,223 @@ for i in range(len(all_data)):
             [all_data[ypar].iloc[i], all_data[ypar].iloc[i]], 
             [all_data['RMSESurr'].iloc[i], all_data['RMSECal'].iloc[i]], c='k', linestyle='--', linewidth=0.5)
 
-ax.legend()
 plt.title('RMSE on All Data')
 ax.set_zlim(0, 0.015)
 ax.set_xlabel(xpar_label)
 ax.set_ylabel(ypar_label)
+
+# Set view angle to 45 degrees from all sides
+ax.view_init(elev=30, azim=-45)
 plt.show()
 #ax.set_ylim(0, 2)
 
+
+#%%
+# Do the same as above for EnergyCal and EnergySur
+
+# Filter only test data
+test_data = surrogate_performance[surrogate_performance['TestOrTrain'] == 'Test']
+# Create a 3D scatter plot of energy_sur
+fig = plt.figure(dpi=dpi_plots, figsize=figuresize)
+ax = fig.add_subplot(111, projection='3d')
+
+# Create a color map based on FailureType
+color_values = test_data['Type'].map(colors)
+
+ax.scatter(test_data[xpar], test_data[ypar], test_data['EnergySurr'], c=color_values, marker='o')
+
+# Add color labels
+for label, color in color_labels_surrogate.items():
+    ax.scatter([], [], c=color, label=f'{label}')
+
+# Add energy_cal
+ax.scatter(test_data[xpar], test_data[ypar], test_data['EnergyCal'], c=color_values, marker='^')
+
+# Add color labels for calibration
+for label, color in color_labels_cals.items():
+    ax.scatter([], [], c=color, marker='^', label=f'{label}')
+
+# Join both with vertical lines
+for i in range(len(test_data)):
+    ax.plot([test_data[xpar].iloc[i], test_data[xpar].iloc[i]], 
+            [test_data[ypar].iloc[i], test_data[ypar].iloc[i]], 
+            [test_data['EnergySurr'].iloc[i], test_data['EnergyCal'].iloc[i]], c='k', linestyle='--', linewidth=0.5)
+
+ax.legend(loc='upper left', fontsize=8)
+plt.title('Energy on Test Data')
+ax.set_xlabel(xpar_label)
+ax.set_ylabel(ypar_label)
+ax.view_init(elev=30, azim=-45)
+plt.show()
+
+# Do same for train data
+train_data = surrogate_performance[surrogate_performance['TestOrTrain'] == 'Train']
+fig = plt.figure(dpi=dpi_plots, figsize=figuresize)
+ax = fig.add_subplot(111, projection='3d')
+
+# Create a color map based on FailureType
+color_values = train_data['Type'].map(colors)
+
+# Create a 3D scatter plot of energy_sur
+ax.scatter(train_data[xpar], train_data[ypar], train_data['EnergySurr'], c=color_values, marker='o')
+
+# Add color labels
+for label, color in color_labels_surrogate.items():
+    ax.scatter([], [], c=color, label=f'{label}')
+
+# Add energy_cal
+ax.scatter(train_data[xpar], train_data[ypar], train_data['EnergyCal'], c=color_values, marker='^')
+
+# Add color labels for calibration
+for label, color in color_labels_cals.items():
+    ax.scatter([], [], c=color, marker='^', label=f'{label}')
+
+# Join both with vertical lines
+for i in range(len(train_data)):
+    ax.plot([train_data[xpar].iloc[i], train_data[xpar].iloc[i]], 
+            [train_data[ypar].iloc[i], train_data[ypar].iloc[i]], 
+            [train_data['EnergySurr'].iloc[i], train_data['EnergyCal'].iloc[i]], c='k', linestyle='--', linewidth=0.5)
+
+ax.legend(loc='upper left', fontsize=8)
+plt.title('Energy on Train Data')
+ax.set_xlabel(xpar_label)
+ax.set_ylabel(ypar_label)
+ax.view_init(elev=30, azim=-45)
+plt.show()
+
+# Do same for all data
+all_data = surrogate_performance
+fig = plt.figure(dpi=dpi_plots, figsize=figuresize)
+ax = fig.add_subplot(111, projection='3d')
+
+# Create a color map based on FailureType
+color_values = all_data['Type'].map(colors)
+
+# Create a 3D scatter plot of energy_sur
+ax.scatter(all_data[xpar], all_data[ypar], all_data['EnergySurr'], c=color_values, marker='o')
+
+# Add color labels
+for label, color in color_labels_surrogate.items():
+    ax.scatter([], [], c=color, label=f'{label}')
+
+# Add energy_cal
+ax.scatter(all_data[xpar], all_data[ypar], all_data['EnergyCal'], c=color_values, marker='^')
+
+# Add color labels for calibration
+for label, color in color_labels_cals.items():
+    ax.scatter([], [], c=color, marker='^', label=f'{label}')
+
+# Join both with vertical lines
+for i in range(len(all_data)):
+    ax.plot([all_data[xpar].iloc[i], all_data[xpar].iloc[i]], 
+            [all_data[ypar].iloc[i], all_data[ypar].iloc[i]], 
+            [all_data['EnergySurr'].iloc[i], all_data['EnergyCal'].iloc[i]], c='k', linestyle='--', linewidth=0.5)
+
+ax.legend(loc='upper left', fontsize=8)
+plt.title('Energy on All Data')
+ax.set_xlabel(xpar_label)
+ax.set_ylabel(ypar_label)
+ax.view_init(elev=30, azim=-45)
+plt.show()
+
+# %%
+# Generate 3D scatter plot
+# Filter only test data
+test_data = surrogate_performance[surrogate_performance['TestOrTrain'] == 'Test']
+# Create a 3D scatter plot of stiff_sur
+fig = plt.figure(dpi=dpi_plots, figsize=figuresize)
+ax = fig.add_subplot(111, projection='3d')
+# Create a color map based on FailureType
+color_values = test_data['Type'].map(colors)
+ax.scatter(test_data[xpar], test_data[ypar], test_data['StiffSur'], c=color_values, marker='o')
+
+# Add color labels
+for label, color in color_labels_surrogate.items():
+    ax.scatter([], [], c=color, label=f'{label}')
+
+# Add stiff_cal
+ax.scatter(test_data[xpar], test_data[ypar], test_data['StiffCal'], c=color_values, marker='^')
+
+# Add color labels for calibration
+for label, color in color_labels_cals.items():
+    ax.scatter([], [], c=color, marker='^', label=f'{label}')
+
+# Join both with vertical lines
+for i in range(len(test_data)):
+    ax.plot([test_data[xpar].iloc[i], test_data[xpar].iloc[i]], 
+            [test_data[ypar].iloc[i], test_data[ypar].iloc[i]], 
+            [test_data['StiffSur'].iloc[i], test_data['StiffCal'].iloc[i]], c='k', linestyle='--', linewidth=0.5)
+
+ax.legend(loc='upper left', fontsize=8)
+plt.title('Stiffness on Test Data')
+ax.set_xlabel(xpar_label)
+ax.set_ylabel(ypar_label)
+ax.view_init(elev=30, azim=-45)
+plt.show()
+
+# Do same for train data
+train_data = surrogate_performance[surrogate_performance['TestOrTrain'] == 'Train']
+fig = plt.figure(dpi=dpi_plots, figsize=figuresize)
+ax = fig.add_subplot(111, projection='3d')
+# Create a color map based on FailureType
+color_values = train_data['Type'].map(colors)
+# Create a 3D scatter plot of stiff_sur
+ax.scatter(train_data[xpar], train_data[ypar], train_data['StiffSur'], c=color_values, marker='o')
+
+# Add color labels
+for label, color in color_labels_surrogate.items():
+    ax.scatter([], [], c=color, label=f'{label}')
+
+# Add stiff_cal
+ax.scatter(train_data[xpar], train_data[ypar], train_data['StiffCal'], c=color_values, marker='^')
+
+# Add color labels for calibration
+for label, color in color_labels_cals.items():
+    ax.scatter([], [], c=color, marker='^', label=f'{label}')
+
+# Join both with vertical lines
+for i in range(len(train_data)):
+    ax.plot([train_data[xpar].iloc[i], train_data[xpar].iloc[i]], 
+            [train_data[ypar].iloc[i], train_data[ypar].iloc[i]], 
+            [train_data['StiffSur'].iloc[i], train_data['StiffCal'].iloc[i]], c='k', linestyle='--', linewidth=0.5)
+
+ax.legend(loc='upper left', fontsize=8)
+plt.title('Stiffness on Train Data')
+ax.set_xlabel(xpar_label)
+ax.set_ylabel(ypar_label)
+ax.view_init(elev=30, azim=-45)
+plt.show()
+
+# Do same for all data
+all_data = surrogate_performance
+fig = plt.figure(dpi=dpi_plots, figsize=figuresize)
+ax = fig.add_subplot(111, projection='3d')
+
+# Create a color map based on FailureType
+color_values = all_data['Type'].map(colors)
+# Create a 3D scatter plot of stiff_sur
+ax.scatter(all_data[xpar], all_data[ypar], all_data['StiffSur'], c=color_values, marker='o')
+
+# Add color labels
+for label, color in color_labels_surrogate.items():
+    ax.scatter([], [], c=color, label=f'{label}')
+
+# Add stiff_cal
+ax.scatter(all_data[xpar], all_data[ypar], all_data['StiffCal'], c=color_values, marker='^')
+
+# Add color labels for calibration
+for label, color in color_labels_cals.items():
+    ax.scatter([], [], c=color, marker='^', label=f'{label}')
+
+# Join both with vertical lines
+for i in range(len(all_data)):
+    ax.plot([all_data[xpar].iloc[i], all_data[xpar].iloc[i]], 
+            [all_data[ypar].iloc[i], all_data[ypar].iloc[i]], 
+            [all_data['StiffSur'].iloc[i], all_data['StiffCal'].iloc[i]], c='k', linestyle='--', linewidth=0.5)
+
+ax.legend(loc='upper left', fontsize=8)
+plt.title('Stiffness on All Data')
+ax.set_xlabel(xpar_label)
+ax.set_ylabel(ypar_label)
+ax.view_init(elev=30, azim=-45)
+plt.show()
